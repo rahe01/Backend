05f5699fee5f132ebd5ecd333f93cb642f147098
Rahat Ahmed Rahe <raheahmed009@email.com>
2025-08-27 00:44:49 +0600

    django-view

---
+++ Views/myapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.demo1),
+    path('demo2/', views.demo2),
+    path('demo3/', views.demo3),
+   
+    path('demo4/', views.demo4),
+    path('demo5/', views.demo5),
+    path('demo6/', views.demo6),
+    
+    path('demo7/', views.demo7),
+    path('demo8/', views.demo8),
+    path('demo9/', views.demo9),
+]
---
+++ Views/myapp/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound , HttpResponseRedirect
+
+# Create your views here.
+
+# - Simple plain text RESPONSE
+def demo1(request):
+    return HttpResponse("Hello World! This is my first Django view.")
+
+
+# - Number RESPONSE
+def demo2(request):
+    return HttpResponse(1000)
+
+
+
+# - Boolead RESPONSE
+def demo3(request):
+    return HttpResponse(None)
+
+
+# - JSON RESPONSE
+def demo4(request):
+    
+    data={
+        "name":"John",
+        "age":30,
+        "city":"New York"
+    }
+    
+    
+    
+    return JsonResponse(data)
+
+
+
+
+
+# - Not Found
+def demo5(request):
+    return HttpResponseNotFound("<h1>404 Not Found</h1><p>The requested resource was not found on this server.</p>")
+
+
+
+
+
+
+
+# - Redirect
+def demo6(request):
+    return HttpResponseRedirect("/")
+
+
+
+
+
+
+
+# - With status code
+def demo7(request):
+    return HttpResponse("This is a custom status code response.", status=500)
+
+
+
+
+
+
+# - With RESPONSE header
+def demo8(request):
+    
+    response = HttpResponse("Hello world" )
+    response["Custom-Header"] = "ABC123"
+    response["Developer"] = "Rahe"
+    response["token"] = "XYZ987"
+    response["Access-Control-Allow-Origin"] = "*"
+    return response
+
+
+
+
+
+
+# - With RESPONSE cookies
+def demo9(request):
+    pass
+
