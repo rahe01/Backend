5d4c537bebc599e303e35388a3d3e73153959dc5
Rahat Ahmed Rahe <rahatrahe01@gmail.com>
2025-08-30 21:46:18 +0600

    model-migration-edit

--- Model Migration/myapp/models.py
+++ Model Migration/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class BookRahe(models.Model):
+    
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    writer = models.CharField(max_length=100)
+    des = models.TextField( null=True)
