a005b44ca8f6f500644e6ea08d8f24fa0ce7de92
rahe01 <rahatrahe01@gmail.com>
2025-09-02 23:33:35 +0600

    default

--- 8.Django Practice/myapp/views.py
+++ 8.Django Practice/myapp/views.py
@@ -12,7 +12,14 @@ def Home(request):
             {"name" : "apple" , "price" : 5000} 
         ],
         "fruits": ["apple" ,"banana" , "orange"  ],
-        "price": [10,23,34,5,6,56]
+        "price": [10,23,34,5,6,56],
+        "des":None,
+        'cars': [
+      {'brand': 'Ford', 'model': 'Mustang', 'year': 1964},
+      {'brand': 'Volvo', 'model': 'XC90', 'year': 2022},
+      {'brand': 'Volvo', 'model': 'P1800', 'year': 1962},
+      {'brand': 'Ford', 'model': 'Focus', 'year': 2004},
+    ]
 
     }
 
