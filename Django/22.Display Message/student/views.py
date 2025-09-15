4beae520f9f1782f7c15187d4b1d9093a9119b0c
rahe01 <rahatrahe01@gmail.com>
2025-09-15 23:31:02 +0600

    book

--- Django/22.Display Message/student/views.py
+++ Django/22.Display Message/student/views.py
@@ -7,6 +7,9 @@ def home(req):
     # messages.add_message(req, messages.SUCCESS, 'Your account has been created')
 
     messages.success(req, 'This is success')
+    
+    messages.set_level(req , messages.DEBUG)
+    messages.debug(req,'thisjisdfsdf')
 
     return render(req, 'home.html')
 
