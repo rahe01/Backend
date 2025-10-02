da6ac801801279fdf0e9de7bb96c9d6f465245a9
rahe01 <rahatrahe01@gmail.com>
2025-10-03 00:13:56 +0600

    delete project

--- Project/Book Review/account/views.py
+++ Project/Book Review/account/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+def signup(request):
+    return render(request, 'account/signup.html')
+
+def signin(request):
+    return render(request, 'account/signin.html')
